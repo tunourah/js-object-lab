95712c81fe96b6a02bc545437ab6129451ae7bb4
noura <noura.tu19@gmail.com>
2024-09-10 11:24:40 +0300

    solve lab 2

--- main.js
+++ main.js
@@ -52,3 +52,72 @@ console.log("print the user skills index 2 :", user.skills[2]);
 
 //9
 console.log("print the user skills  :", user.skills.join(" , "));
+
+
+
+
+// ---------------------------------------Lab 2--------------------------------------------
+
+
+characters = [
+    {
+      name: "Luke Skywalker",
+      height: 177,
+      gender: "male",
+      mass: 77,
+      eye_color: "brown",
+    },
+    {
+      name: "Leia Organa",
+      height: 160,
+      gender: "female",
+      mass: 56,
+      eye_color: "blue",
+    },
+    {
+      name: "Han Solo",
+      height: 180,
+      gender: "male",
+      mass: 80,
+      eye_color: "brown",
+    },
+    {
+      name: "Chewie",
+      height: 222,
+      gender: "male",
+      mass: 190,
+      eye_color: "black",
+    },
+    {
+      name: "kevien",
+      height: 106,
+      gender: "male",
+      mass: 32.2,
+      eye_color: "red",
+    },
+  ];
+
+console.log("Get first name that has blue eyes :"
+, characters.find((element) => element.eye_color === "blue").name);
+
+console.log("Get first gender that is his mass is over 50 :" , characters.find((element) => element.mass > 50).gender);
+
+console.log("Get characters with height less than 200", characters.filter((element) => element.height < 200));
+
+console.log("Get all male characters", characters.filter((element) => element.gender === "male"));
+
+console.log("Get array of all names only :", characters.map((element) => element.name));
+
+console.log("Get array of all heights only:", characters.map((element) => element.height));
+
+console.log(" Sort by mass (low to high) :", characters.sort((a, b) => a.mass - b.mass));
+
+console.log("Sort by height (high to low) :", characters.sort((a, b) => b.height - a.height));
+
+console.log("Does every character have mass more than 40?", characters.map((element) => element.mass > 40));
+
+console.log("-Is every character shorter than 200? ", characters.map((element) => element.height < 200));
+
+console.log("Is there at least one character with blue eyes?", characters.map((element) => element.eye_color === "blue"));
+
+console.log("Is there at least one character taller than 210?", characters.map((element) => element.height > 210));
