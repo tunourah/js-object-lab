68ed5e01cf3269ba53e2737f049b20b5ccb18c54
noura <noura.tu19@gmail.com>
2024-09-10 10:53:10 +0300

    slove the lab

---
+++ main.js
@@ -0,0 +1,49 @@
+const user = {
+    name: "nora",
+    email: "nourahaltuaim@gmail.com",
+    id: 1,
+    avalable: false,
+    check : function ()  {
+        return this.avalable;
+    }, 
+
+    address: {
+        street: "1234",
+        city: {
+            name: "Riyadh",
+            zip: 12345,
+        },
+        country: "KSA",
+    },
+    skills: ["html", "css", "js"],
+
+};
+//1
+console.log("print the object user :", user);
+
+//2 
+console.log("print the user name :", user.name);
+
+//3
+user.id = 2;
+console.log("print the user id after update :", user.id);
+
+//4
+delete user.email;
+console.log("print the user email after delete :", user.email);
+
+//5
+user.avalable = true;
+console.log("print the user avalable after update :", user.avalable);
+
+//6
+console.log("print the user address :", user.address);
+
+//7
+console.log("print the user city name :", user.address.city.name);
+
+//8
+console.log("print the user skills index 2 :", user.skills[1]);
+
+//9
+console.log("print the user skills  :", user.skills.join(" , "));
